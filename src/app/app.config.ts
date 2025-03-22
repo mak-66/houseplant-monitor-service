98c9e7e8cfd30c6758e38bccfac343deb74057a1
Musa Kwong <122374907+mak-66@users.noreply.github.com>
2025-03-22 10:23:41 -0400

    init firebase linking

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,10 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"houseplant-monitor-service","appId":"1:61251023349:web:dc57962c2325ff105a30bc","storageBucket":"houseplant-monitor-service.firebasestorage.app","apiKey":"AIzaSyAfIoN9rjVlHPs9bcbR-_lE6KJCaWrd2qE","authDomain":"houseplant-monitor-service.firebaseapp.com","messagingSenderId":"61251023349","measurementId":"G-L9K85C86J9"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
 };
