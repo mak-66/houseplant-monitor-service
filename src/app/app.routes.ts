c5f963391c46e55d094fe4e872b1065a934786e5
kjan1 <krj26@calvin.edu>
2025-03-22 03:34:56 -0400

    gallery

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { GalleryComponent } from './components/gallery/gallery.component';
 export const routes: Routes = [
     { path: '', component: LoginComponent },
     { path: 'gallery', component: GalleryComponent },
-    
+
     { path: '**', redirectTo: '' }
     
 ]; 
