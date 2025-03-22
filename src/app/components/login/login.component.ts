2fc14ed32e332027ba606d1e52979083469d234f
Musa Kwong <122374907+mak-66@users.noreply.github.com>
2025-03-22 12:00:18 -0400

    login inputs implemented

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,17 +1,21 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
   router = inject(Router);
+  username: string = ''; // Variable for username
+  password: string = ''; // Variable for password
 
   async onLogin(): Promise<void> {
+    
     this.router.navigate(['/gallery']);  // Navigate to the profile page
   }
 }
